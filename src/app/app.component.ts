aca48c5e0ef06a26fe581135bf8578302409d08b
shielama09 <shielamaenarido09@gmail.com>
2022-01-06 23:35:00 +0800

    Well done baby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'examCalcangular';
+
+ num1:number = 0
+ num2:number = 0
+ num3:number = 0
+
+add(){
+  this.num3=this.num1+this.num2;
+}
+sub(){
+  this.num3=this.num1-this.num2;
+}
+multiply(){
+  this.num3=this.num1*this.num2;
+}
+divide(){
+  this.num3=this.num1/this.num2;
+}
+
 }
